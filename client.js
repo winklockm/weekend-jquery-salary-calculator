66cbc12897881bb1f8cbfff2c7d8fc64aca8952f
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-27 12:23:47 -0500

    add total monthly to footer

--- client.js
+++ client.js
@@ -7,6 +7,24 @@ function onReady(){
 
 let employeeArray = [];
 
+
+// function handleSubmit(){  // creates object and pushes it into array
+//     let firstNameInput = $('#firstname-input').val();
+//     let lastNameInput = $('#lastname-input').val();
+//     let idNumInput = $('#id-input').val();
+//     let titleInput = $('#title-input').val();
+//     let salaryInput = $('#salary-input').val();
+//     let employee = {
+//         firstname: firstNameInput,
+//         lastname: lastNameInput,
+//         idnumber: idNumInput,
+//         title: titleInput,
+//         salary: salaryInput,
+//     }
+//     employeeArray.push(employee); //push employee object into array
+//     $('#datatable').append(`<tr><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idNumInput}</td><td>${titleInput}</td><td>${salaryInput}</td></tr>`);
+// }
+
 function handleSubmit(){  // creates object and pushes it into array
     let firstNameInput = $('#firstname-input').val();
     let lastNameInput = $('#lastname-input').val();
@@ -21,7 +39,16 @@ function handleSubmit(){  // creates object and pushes it into array
         salary: salaryInput,
     }
     employeeArray.push(employee); //push employee object into array
-    $('#datatable').append(`<tr><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idNumInput}</td><td>${titleInput}</td><td>${salaryInput}</td></tr>`);
+    calculateTotalFromArray(employeeArray);
+
+    // below adds that to the table but can I do it by pulling from the array
+    // $('#datatable').append(`<tr><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idNumInput}</td><td>${titleInput}</td><td>${salaryInput}</td></tr>`);
+}
+
+
+function calculateTotalFromArray(array){
+    console.log('in pull from array');
+
 }
 
 // take the last item's info out of array and append it into table
